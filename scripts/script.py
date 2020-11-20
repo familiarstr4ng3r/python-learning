15c89a298cbf641692be31004ad0c885f6306ce2
FamiliarStrang3r <53304126+FamiliarStrang3r@users.noreply.github.com>
2020-11-20 23:19:39 +0200

    example code hints

---
+++ scripts/script.py
@@ -0,0 +1,15 @@
+def func(lat1: float, lon1: float, lat2: float, lon2: float) -> float:
+    """
+    Desctipthion provided here.
+    
+    Args:
+        lat1, lon1: latitude and longitude of coordinate 1
+        lat2, lon2: latitude and longitude of coordinate 2
+    Returns:
+        geographical distance between two points in metres
+
+    """
+
+    return 0
+
+f = func(0, 0, 0, 0)
