f848d99105835d686bf3b575d9cb234e17409223
FamiliarStrang3r <53304126+FamiliarStrang3r@users.noreply.github.com>
2021-01-01 16:11:42 +0200

    app: atlas packer minor refactroing

--- Applications/Atlas Packer/auto_packer.py
+++ Applications/Atlas Packer/auto_packer.py
@@ -1,4 +1,3 @@
-import os
 import json
 import atlas_packer
 
@@ -13,24 +12,36 @@ def get_text(path):
 
 
 def handle_dict(dictionary):
+
+    separator = "/"
+    
     folders = dictionary["folders"]
     save_path = dictionary["save_path"]
+    base_path = dictionary["base_path"]
 
     for folder in folders:
-        images = atlas_packer.get_images(folder["path"])
-        x, y = folder["grid_x"], folder["grid_y"]
-        div = folder["divisibility"]
-        result = atlas_packer.pack_grid(images, x, y)
-        #atlas_packer.show(result, 0.2)
+        path = separator.join([base_path, folder["path"]])
+        images = atlas_packer.get_images(path)
 
-        path = os.path.join(save_path, folder["filename"])
-        result.save(result, path)
-        #print(path)
+        grid = folder["grid"]
+        pivot = folder["pivot"]
+        #div = folder["divisibility"]
+        same_size = folder["same_size"]
+
+        frame_size = None if same_size else atlas_packer.get_max_size(images)
+        
+        result = atlas_packer.pack_grid(images, grid, pivot = pivot, frame_size = frame_size)
+
+        path = separator.join([save_path, folder["filename"]])
+        result.save(path)
+        
+        #result = atlas_packer.draw_rects(result, grid)
+        #atlas_packer.show(result)
 
 
 def main():
 
-    path = "resources/prehistoric/trees.json"
+    path = "Resources/prehistoric.json"
 
     if path is None: path = input("Enter path to json: ")
 
@@ -38,11 +49,10 @@ def main():
 
     if success:
         dictionary = json.loads(text)
-        print(dictionary)
-        #handle_dict(dictionary)
+        #print(dictionary)
+        handle_dict(dictionary)
     else:
         print(error)
-        main()
 
 
 if __name__ == "__main__":
